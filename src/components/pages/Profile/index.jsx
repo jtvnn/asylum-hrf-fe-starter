7f0e436a1587c5109f86ceafac6a730d5356303c
Jason Vann <vannjt1@icloud.com>
2025-12-10 17:23:52 -0500

    fixed profile page

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -21,7 +21,23 @@ const Profile = () => {
     return <Navigate to='/' replace />;
   }
 
-  return <div>Profile Page</div>;
+  return (
+    <div className='flex flex-col items-center justify-center p-8 min-h-[60vh]'>
+      <div className='bg-white rounded-lg shadow-lg p-8 max-w-md w-full'>
+        <div className='flex flex-col items-center'>
+          {user?.picture && (
+            <img
+              src={user.picture}
+              alt={user.name}
+              className='w-32 h-32 rounded-full mb-4 border-4 border-blue-500'
+            />
+          )}
+          <h1 className='text-3xl font-bold mb-2'>{user?.name}</h1>
+          <p className='text-gray-600 mb-4'>{user?.email}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
