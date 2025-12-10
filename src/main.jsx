2b5cda500810c93220b8d896bc85f2094bb2c940
Jason Vann <vannjt1@icloud.com>
2025-12-09 22:07:34 -0500

    Start Ticket 3: Auth & loggiing w/Auth0

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import './index.css';
 import { App } from './App.jsx';
 import { ProvideAppContext } from './context/AppContext.jsx';
 
+
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
 const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
 
@@ -14,7 +15,15 @@ const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
  * - Set the domain, clientId, and authorizationParams
  */
 createRoot(document.getElementById('root')).render(
+  <Auth-Provider
+  domain={AUTH_DOMAIN}
+  clientId={AUTH_CLIENT_ID}
+  authorizationParams={{
+    redirect_uri: window.location.origin,
+  }}
+  >
     <ProvideAppContext>
       <App />
     </ProvideAppContext>
+    </Auth-Provider>
 );
