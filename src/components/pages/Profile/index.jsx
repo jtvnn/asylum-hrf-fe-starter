0c84574f518cfbebda1e2d38121b02775bfe8206
Jason Vann <vannjt1@icloud.com>
2025-12-10 17:05:40 -0500

    implement auth0 in main and index

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -6,18 +6,22 @@
  * - Display the data
  * - Make this page a protected Route
  */
+import { useAuth0 } from '@auth0/auth0-react';
+import { Navigate } from 'react-router-dom';
+
 const Profile = () => {
   // TODO: Replace these with functionality from Auth0
-  const isLoading = false;
-  const user = true;
+  const { user, isLoading, isAuthenticated } = useAuth0();
 
-  if (isLoading || !user) {
+  if (isLoading) {
     return <div className='text-center p-4'>Loading...</div>;
   }
 
-  return (
-    <div>Profile Page</div>
-  );
+  if (!isAuthenticated) {
+    return <Navigate to='/' replace />;
+  }
+
+  return <div>Profile Page</div>;
 };
 
 export default Profile;
